28828a1db3a7463eeb093727698927ac03049c54
Pumlani <lani.buhle@gmail.com>
2018-07-31 13:31:10 +0200

    commit with figlet

--- greet_xola.js
+++ greet_xola.js
@@ -1,7 +1,19 @@
 
 const chalk = require('chalk');
 //import the greet module that is in the current folder
-const greet = require('./greet');
+const hello = require('./greet');
 
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
+const styledMessage = chalk.bgGreen.black(hello('Lani'));
 console.log(styledMessage)
+
+var figlet = require('figlet');
+var greet = require('./greet');
+
+figlet('Hello Lani!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
